02f6d06817864d9f0aadd19b4ef4663efcb217df
EC2 Default User <ec2-user@ip-172-31-81-173.ec2.internal>
2020-05-11 10:24:23 +0000

    day1 docker file

---
+++ day1/pyapp/hello.py
@@ -0,0 +1,7 @@
+import  time
+while 4 > 2:
+    print("Hello world  welcome to python 3")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@@                           @@@@@")
+    time.sleep(2)
